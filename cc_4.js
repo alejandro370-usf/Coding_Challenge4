d1b948b88489975def016bea52782c6895887f5d
Alejandro Pineda <alejandro370@usf.edu>
2025-06-15 11:01:21 -0400

    Added Array of Products

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+
+// Array of products
+const products = [
+  { name: "Laptop", category: "electronics", price: 1000, inventory: 5 },
+  { name: "Jacket", category: "apparel", price: 25, inventory: 10 },
+  { name: "Chicken Breast", category: "groceries", price: 1, inventory: 100 },
+  { name: "Bounty Wipes", category: "household", price: 5, inventory: 50 },
+  { name: "School Textbooks", category: "School Textbooks", price: 15, inventory: 20 },
+];
