f0e7fa914c29cbd6603e9ac2f989e32f08357a3c
Alejandro Pineda <alejandro370@usf.edu>
2025-06-15 11:07:40 -0400

    Discount based on category

--- cc_4.js
+++ cc_4.js
@@ -7,3 +7,23 @@ const products = [
   { name: "Bounty Wipes", category: "household", price: 5, inventory: 50 },
   { name: "School Textbooks", category: "School Textbooks", price: 15, inventory: 20 },
 ];
+// Discount based on category
+for (const product of products) {
+  switch (product.category) {
+    case "electronics":
+      product.price *= 0.8; // 20% off
+      break;
+    case "apparel":
+      product.price *= 0.85; // 15% off
+      break;
+    case "groceries":
+    case "household":
+      product.price *= 0.9; // 10% off
+      break;
+    default:
+      // No discount
+      break;
+  }
+  // Log new price after category discount
+  console.log(`Discounted price for ${product.name}: $${product.price.toFixed(2)}`);
+}
